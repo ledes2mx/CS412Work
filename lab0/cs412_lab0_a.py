4319dc03aa2c7d08a06b0b56990758a24a5ded19
ledes2mx <ledes2mx@dukes.jmu.edu>
2022-01-21 22:38:22 -0500

    redid lab 0

--- lab0/cs412_lab0_a.py
+++ lab0/cs412_lab0_a.py
@@ -1,14 +1,24 @@
 """
-    name:  Your name(s) here
+    name:  Miguel Ledesma & Quade Curry
     
 """
 # All modules for CS 412 must include a main method that allows it
 # to imported and invoked from other python scripts
+# call input to read next line
+
 def main():
+    words = {}
     n = int(input())
     lines = [input().split(" ") for _ in range(n)]
     for line in lines:
-        print(line[0], ":", line[1])
+        words[line[1]] = line[0]
+    last = input().split(" ")
+    for word in last:
+        if word in words:
+            print(words[word], end=' ')
+        else:
+            print("???", end=' ')
+    print()
 
 if __name__ == "__main__":
     main()
