49bcd26585d0eb41f703b7cfceecdfba40469e5b
ledes2mx <ledes2mx@dukes.jmu.edu>
2022-01-23 18:48:54 -0500

    halfway done with B

--- homework0/cs412_hw0_b.py
+++ homework0/cs412_hw0_b.py
@@ -1,1 +1,22 @@
-# DICTIONARIES ALLOWED
+"""
+    name: Miguel Ledesma
+
+"""
+# All modules for CS412 must include a main method that allows it
+# to imported and invoked from other python scripts# DICTIONARIES ALLOWED
+
+def main():
+    animal_sound = {}
+    heard = {}
+    sounds = input().split(" ")
+    n = int(input())
+    lines = [input().split(" ") for _ in range(n)]
+    for line in lines:
+        animal_sound[line[2]] = line[0]
+    for sound in sounds:
+        if sound in animal_sound and sound not in heard:
+            heard[animal_sound[sound]] = 1
+    print(heard)
+
+if __name__ == "__main__":
+    main()
