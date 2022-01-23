c160dff58f4e1ce6763d3a3cdf69d04adfff2afd
ledes2mx <ledes2mx@dukes.jmu.edu>
2022-01-22 20:42:50 -0500

    Done with the lab

--- lab0/cs412_lab0_a.py
+++ lab0/cs412_lab0_a.py
@@ -1,5 +1,17 @@
 """
-    name:  Miguel Ledesma & Quade Curry
+    name:  Miguel Ledesma
+
+    1. I used a dictionary as my only data structure.
+
+    2. I used a store and a lookup
+
+    3. Both are O(1) worst case since lookups and storage are both constant time operations.
+
+    4. O(m + n) because m is the storage of the key and m is the lookup of a word. these two happen at different times.
+
+    5. It works because the input is always going to be in the same format, that being a number, (for the number of lines to add to the dictionary)
+    the lines actually added to the dictionary, then the words to look up in the dictionary at the end.
+
     
 """
 # All modules for CS 412 must include a main method that allows it
