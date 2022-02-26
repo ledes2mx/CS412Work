e9df44f660777d23b6ab08ea0c971789cebebd6f
ledes2mx <ledes2mx@dukes.jmu.edu>
2022-02-25 21:11:03 -0500

    another go

--- lab6/cs412_lab6_a.py
+++ lab6/cs412_lab6_a.py
@@ -7,23 +7,26 @@ def isPalindrone(s):
     return s == s[::-1]
 def countPalindrones(word):
     optimal_counts = {}
-    storage = [0] * (len(word) + 1)
-    storage[len(word)] = 1
-    #print(storage)
+    storage = [[0]*(len(word)+1)]*(len(word)+1)
+    print(storage)
     def cPalindrones(start):
-        """
-        count = 0
         if start == len(word):
             return 1
         for i in range(start, len(word)):
-            if isPalindrone(word[start:i+1]):
-                if i+1 not in optimal_counts:
-                    optimal_counts[i+1] = cPalindrones(i+1)
-                count += optimal_counts[i+1]
-
-        print(optimal_counts)
-        return count
+            for j in range(i +1, len(word)):
+                if isPalindrone(word[i:j]):
+                    storage[i][j] += 1
+                if storage[i][i] != 1:
+                    storage[i][j] = (storage[i][j-1] + storage[i-1][j] + 1 - storage[i-1][j-1])
+                print(storage)
+        return storage[len(word)-1][len(word)-1]
+        """"
+        if isPalindrone(word[start:i+1]):
+            if i+1 not in optimal_counts:
+                optimal_counts[i+1] = cPalindrones(i+1)
+            count += optimal_counts[i+1]
         """
+        #return count
         """
         DOESNT WORK FOR THE TEST BUB
         for i in range(len(word)-1, -1, -1):
@@ -35,7 +38,7 @@ def countPalindrones(word):
         
         return (storage[0] + 1)
         """
-        
+        """
         #NOT REALLY A LOOKUP ??? 
         for i in range(start, len(word)):
             if i != 0:
@@ -44,7 +47,7 @@ def countPalindrones(word):
                 if isPalindrone(word[i:j]):
                     storage[i] += 1
         return storage[len(word)-1]
-        
+        """
     return cPalindrones(0)
 def main():
     n = int(input())
