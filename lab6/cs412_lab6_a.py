3f32aa459f279c0a4f0fc176db812bae3b650b7b
ledes2mx <ledes2mx@dukes.jmu.edu>
2022-02-25 19:55:13 -0500

    try 2

--- lab6/cs412_lab6_a.py
+++ lab6/cs412_lab6_a.py
@@ -5,6 +5,9 @@ def isPalindrone(s):
     return s == s[::-1]
 def countPalindrones(word):
     optimal_counts = {}
+    storage = [0] * (len(word) + 1)
+    storage[len(word)] = 1
+    #print(storage)
     def cPalindrones(start):
         """
         count = 0
@@ -15,38 +18,28 @@ def countPalindrones(word):
                 if i+1 not in optimal_counts:
                     optimal_counts[i+1] = cPalindrones(i+1)
                 count += optimal_counts[i+1]
-        """
-        #return count
-        storage = [0] * (len(word)+1)
-        #print(storage)
-        #check if value is not in dictionary, if not, add it to dictionary
-        if 1 >= len(word):
-            return 1
-        #for i in range(start, len(word)+1):
-        for i in range(-1, -(len(word)+1), -1):
-            #print(i)
-            for j in range(len(word)+i, len(word)):
-                #print(word[i:j+1])
-                if len(word[i:j+1]) == 1:
-                    if word[i:j+1] not in optimal_counts.values():
-                        #print("NOT IN YET")
-                    #print(word[i:j+1])
-                        optimal_counts[j] = word[i:j+1]
-                        storage[j] += storage[j+1] + 1
-                        #print(storage)
 
-                    #print(j)
-                    else:
-                        storage[j] = storage[j+1]
-                        #print(storage)
-                elif isPalindrone(word[i:j+1]):
-                    #print("IS PALINDROME")
-                    storage[i-1] += 1
-            #print(word[i:])
-        #print(len(storage))
-        if len(storage)-1 == len(optimal_counts):
-            storage[0] = storage[0] - (len(optimal_counts) -1) 
-        return storage[0]
+        print(optimal_counts)
+        return count
+        """
+        for i in range(len(word)-1, -1, -1):
+            storage[i] = storage[i+1]
+            for j in range(i+1, len(word) + 1):
+                if len(word[i:j]) > 1:
+                    if isPalindrone(word[i:j]):
+                        storage[i] += 1
+        
+        return (storage[0] + 1)
+        """
+        NOT REALLY A LOOKUP
+        for i in range(start, len(word)):
+            if i != 0:
+                storage[i] = storage[i-1]
+            for j in range(i+1, len(word)):
+                if isPalindrone(word[i:j]):
+                    storage[i] += 1
+        return storage[len(word)-1]
+        """
     return cPalindrones(0)
 def main():
     n = int(input())
