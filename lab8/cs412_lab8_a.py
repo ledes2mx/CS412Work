b9edf6a555fa5d43d87a806c75173f23864c7550
ledes2mx <ledes2mx@dukes.jmu.edu>
2022-03-11 15:43:13 -0500

    this might work

--- lab8/cs412_lab8_a.py
+++ lab8/cs412_lab8_a.py
@@ -1,9 +1,51 @@
+from collections import deque
+
+
 def main():
     numInputs = int(input())
     routes = {}
     for i in range(numInputs):
         leave, to = input().split(" ")
-        print(leave, to)
+        #print(leave, to)
+        if leave not in routes:
+            routes[leave] = {to}
+        else:
+            routes[leave].add(to)
+        if to not in routes:
+            routes[to] = {leave}
+        else:
+            routes[to].add(leave)
+    init, finish = input().split(" ")
+    #print(routes)
 
+    visited = set()
+
+    def findRoutes():
+        stack = deque([(0,init)])
+        path = deque([])
+        while len(stack) != 0:
+            print(stack)
+            past, current = stack[-1]
+            stack.pop()
+            if current not in visited:
+                path.append(current)
+                visited.add(current)
+                for nextStop in routes[current]:
+                    stack.append((current,nextStop))
+        #print("PATH", path)
+        return path
+        
+
+
+    route = findRoutes()
+    if finish not in route:
+        print("no route possible")
+    else:
+        for path in route:
+            if path != route[-1]:
+                print(path, end=" ")
+            else:
+                print(path)
+    
 if __name__ == "__main__":
     main()
